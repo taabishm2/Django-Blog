cafcd258212fc120a99fe142e1d13ff7b30e62cf
Tabish <taabishm2@gmail.com>
2019-06-24 14:01:43 +0530

    Data to HTML

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author':'Tabish',
+        'title':'First Post',
+        'content':'First Post Content',
+        'date_posted':'June 23, 2019'
+    },
+    {
+        'author':'Mehvish',
+        'title':'Mehvish Post',
+        'content':'Mehvishs Content',
+        'date_posted':'June 28, 2019'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
